1f1a9bd8c3b406c6e6632e7bd81c90dc9c7e113c
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-02-12 15:35:05 +0300

    Add files via upload
    Nesne Yonelimli Programlama (Object-Oriented Programming)--- 4-> Yerel ve Global tanımlamalar

---
+++ 4.Yerel Ve Global tanımları.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void musa()
+{
+	int x=5;
+	
+	cout<<"x:"<<x<<endl;
+}
+
+int main ()
+{
+	int x=10;
+	
+	cout<<"x:"<<x<<endl;
+	
+	musa();
+	
+	cout<<"x:"<<x<<endl;
+	
+	return 0;
+}
